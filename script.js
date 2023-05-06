089735ce94812b77fd8f645007af6901a103c449
Raj Dave <10864@bpsdoha.edu.qa>
2023-05-06 13:31:40 +0300

    major improvements and adding low res and higher res images

--- script.js
+++ script.js
@@ -1,35 +1,36 @@
-function populateImageGallery(image, title) {
-  /*
-  <div class="image-box">
-    <img src="images/img-1.jpg" alt="img.jpg" />
-  </div>
-  */
-
+function populateImageGallery(low_res_image, high_res_image, title) {
+  // Create a container for the image
   const imageBox = document.createElement("div");
   imageBox.classList.add("image-box");
-  const _image = document.createElement("a");
-  _image.href = "./images/" + image;
-  _image.setAttribute("data-lightbox", `image-${image}`);
-  _image.setAttribute("data-title", title || image);
-  const img = document.createElement("img");
-  img.src = "./images/" + image;
-  img.alt = image;
-  _image.appendChild(img);
 
+  // Create a link to the high-resolution image with a title
+  const _image = document.createElement("a");
+  _image.href = `./images/highres/${high_res_image}`;
+  _image.setAttribute("data-lightbox", `image-${high_res_image}`);
+  _image.setAttribute("data-title", title || low_res_image);
+
+  // Create an image element with a low-resolution source and alt text
+  const img = document.createElement("img");
+  img.src = `./images/lowres/${low_res_image}`;
+  img.alt = low_res_image;
+
+  // Append the image to the link and the link to the container
+  _image.appendChild(img);
   imageBox.appendChild(_image);
 
+  // Return the container
   return imageBox;
 }
 
-document.addEventListener("DOMContentLoaded", function () {
+// Wait for the DOM to be fully loaded before running the code
+document.addEventListener("DOMContentLoaded", function() {
   console.log("DOM fully loaded and parsed");
-  let images = [];
-  for (let i = 1; i < 90; i++) {
-    images.push("f (" + i + ").jpg");
-  }
 
-  images.forEach((image) => {
-    const imageBox = populateImageGallery(image);
-    document.getElementById("image-gallery").appendChild(imageBox);
-  });
+  // Loop through the image filenames in reverse order and create an image gallery for each one
+  for (let i = 101; i > 0; i--) {
+    const image = `${i}.webp`;
+    const high_res_image = `${i}.jpg`;
+    const imageBox = populateImageGallery(image, high_res_image);
+    document.querySelector("#image-gallery").appendChild(imageBox);
+  }
 });
