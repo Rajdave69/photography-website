29ec69692ec2ec577d2de59735f42cf90ac9357f
Raj Dave <10864@bpsdoha.edu.qa>
2023-07-24 11:34:40 +0300

    add fallback to highres image

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function loadImage(image) {
   return new Promise((resolve, reject) => {
     const img = new Image();
-    img.src = `./images/lowres/${image}`;
+    img.src = image;
     img.onload = () => resolve(img);
     img.onerror = reject;
   });
@@ -14,21 +14,30 @@ function populateImageGallery(low_res_image, high_res_image, title) {
 
   // Create a link to the high-resolution image with a title
   const _image = document.createElement("a");
-  _image.href = `./images/highres/${high_res_image}`;
   _image.setAttribute("data-lightbox", `image-${high_res_image}`);
   _image.setAttribute("data-title", title || low_res_image);
 
 
-    loadImage(low_res_image)
+  loadImage(`./images/lowres/${low_res_image}`)
     .then((img) => {
       _image.appendChild(img);
     })
     .catch(() => {
       const imgPlaceholder = document.createElement("img");
-      imgPlaceholder.src = "assets/placeholder.png";
+      imgPlaceholder.src = "./assets/placeholder.png";
       imgPlaceholder.alt = low_res_image;
       _image.appendChild(imgPlaceholder);
     });
+
+  loadImage(`./images/highres/${high_res_image}`)
+    .then((img) => {
+      _image.href = `./images/highres/${high_res_image}`;
+    })
+    .catch(() => {
+        _image.href = "./assets/placeholder.png";
+    });
+
+
 
   // Append the link to the container
   imageBox.appendChild(_image);
